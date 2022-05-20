f1c014cbffc27860243c7db43e9e2b8c68308ebb
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-20 17:41:14 +0600

    connected bd

--- index.js
+++ index.js
@@ -10,8 +10,18 @@ app.use(cors());
 app.use(express.json())
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.zvnon.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+  console.log('db connectedss')
+});
+
 app.get('/',(req,res)=>{
-    res.send('running genuse server')
+    res.send('running clean co server')
 })
 
 
