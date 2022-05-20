31752ab327fc2d732e5aa346d18088fcf57a31a9
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-05-20 18:35:36 +0600

    add get api for load data

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port=process.env.PORT || 5000;
 const app=express()
 
 
 //meddle ware
-app.use(cors());
+app.use(cors({
+    origin:"*",
+}));
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.zvnon.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-  console.log('db connectedss')
-});
+
+async function run(){
+    try{
+        await client.connect();
+        const servicesCollection=client.db("cleanCo").collection("services")
+        console.log("DB Connected sss")
+
+        app.get('/service',async(req,res)=>{
+            
+            const services=await servicesCollection.find({}).toArray()
+            res.send(services)
+        })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir)
 
 app.get('/',(req,res)=>{
     res.send('running clean co server')
